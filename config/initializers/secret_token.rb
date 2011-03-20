5572695884579cdc5ea656049bd3c58360fed1a1
unknown <kim@.(none)>
2011-03-19 21:05:46 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ftree::Application.config.secret_token = '2f14c4ee5667899d1f1cb5ec59fe7bd0495ab52c411c56e48b12c3e16c320b0be70de35f4d802aca566bf595c015211a1525c23a072455e282bd1e4ba7f8082d'
